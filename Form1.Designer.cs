0e50b63cef926ff0054146eadded968ae69dfd60
Jeferson Velásquez <45672904+JefersonV@users.noreply.github.com>
2024-11-22 14:21:41 -0600

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,116 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabControl1 = new TabControl();
-            no = new TabPage();
-            idTarjeta = new TabPage();
-            propietario = new TabPage();
-            presente = new TabPage();
-            horaEntrada = new TabPage();
-            horaSalida = new TabPage();
-            isManual = new TabPage();
-            rechazados = new TabPage();
             label1 = new Label();
             button1 = new Button();
             abrirPuertaBtn = new Button();
             cerrarPuertaBtn = new Button();
-            tabControl1.SuspendLayout();
+            dataGridView1 = new DataGridView();
+            lbltest = new Label();
+            btnUpdataData = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            tabControl1.Controls.Add(no);
-            tabControl1.Controls.Add(idTarjeta);
-            tabControl1.Controls.Add(propietario);
-            tabControl1.Controls.Add(presente);
-            tabControl1.Controls.Add(horaEntrada);
-            tabControl1.Controls.Add(horaSalida);
-            tabControl1.Controls.Add(isManual);
-            tabControl1.Controls.Add(rechazados);
-            tabControl1.Location = new Point(80, 95);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(880, 331);
-            tabControl1.TabIndex = 0;
-            // 
-            // no
-            // 
-            no.Location = new Point(4, 29);
-            no.Name = "no";
-            no.Padding = new Padding(3);
-            no.Size = new Size(872, 298);
-            no.TabIndex = 0;
-            no.Text = "#";
-            no.UseVisualStyleBackColor = true;
-            no.Click += tabPage1_Click;
-            // 
-            // idTarjeta
-            // 
-            idTarjeta.Location = new Point(4, 29);
-            idTarjeta.Name = "idTarjeta";
-            idTarjeta.Padding = new Padding(3);
-            idTarjeta.Size = new Size(872, 298);
-            idTarjeta.TabIndex = 1;
-            idTarjeta.Text = "Id Tarjeta";
-            idTarjeta.UseVisualStyleBackColor = true;
-            // 
-            // propietario
-            // 
-            propietario.Location = new Point(4, 29);
-            propietario.Name = "propietario";
-            propietario.Size = new Size(872, 298);
-            propietario.TabIndex = 2;
-            propietario.Text = "Propietario";
-            propietario.UseVisualStyleBackColor = true;
-            // 
-            // presente
-            // 
-            presente.Location = new Point(4, 29);
-            presente.Name = "presente";
-            presente.Size = new Size(872, 298);
-            presente.TabIndex = 5;
-            presente.Text = "Presente";
-            presente.UseVisualStyleBackColor = true;
-            presente.Click += fecha_Click;
-            // 
-            // horaEntrada
-            // 
-            horaEntrada.Location = new Point(4, 29);
-            horaEntrada.Name = "horaEntrada";
-            horaEntrada.Padding = new Padding(3);
-            horaEntrada.Size = new Size(872, 298);
-            horaEntrada.TabIndex = 3;
-            horaEntrada.Text = "Hora entrada";
-            horaEntrada.UseVisualStyleBackColor = true;
-            // 
-            // horaSalida
-            // 
-            horaSalida.Location = new Point(4, 29);
-            horaSalida.Name = "horaSalida";
-            horaSalida.Padding = new Padding(3);
-            horaSalida.Size = new Size(872, 298);
-            horaSalida.TabIndex = 4;
-            horaSalida.Text = "Hora Salida";
-            horaSalida.UseVisualStyleBackColor = true;
-            // 
-            // isManual
-            // 
-            isManual.BackColor = Color.OrangeRed;
-            isManual.Location = new Point(4, 29);
-            isManual.Name = "isManual";
-            isManual.RightToLeft = RightToLeft.Yes;
-            isManual.Size = new Size(872, 298);
-            isManual.TabIndex = 6;
-            isManual.Text = "Manual";
-            // 
-            // rechazados
-            // 
-            rechazados.BackColor = Color.SkyBlue;
-            rechazados.Location = new Point(4, 29);
-            rechazados.Name = "rechazados";
-            rechazados.Size = new Size(872, 298);
-            rechazados.TabIndex = 7;
-            rechazados.Text = "rechazados";
             // 
             // label1
             // 
@@ -148,7 +47,6 @@
             label1.Size = new Size(183, 31);
             label1.TabIndex = 2;
             label1.Text = "Control Peatonal";
-            label1.Click += label1_Click;
             // 
             // button1
             // 
@@ -185,38 +83,79 @@
             cerrarPuertaBtn.UseVisualStyleBackColor = false;
             cerrarPuertaBtn.Click += cerrrPuertaBtn_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(76, 112);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(775, 188);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // lbltest
+            // 
+            lbltest.AutoSize = true;
+            lbltest.Location = new Point(123, 371);
+            lbltest.Name = "lbltest";
+            lbltest.Size = new Size(50, 20);
+            lbltest.TabIndex = 6;
+            lbltest.Text = "label2";
+            lbltest.Click += lbltest_Click;
+            // 
+            // btnUpdataData
+            // 
+            btnUpdataData.Location = new Point(821, 457);
+            btnUpdataData.Name = "btnUpdataData";
+            btnUpdataData.Size = new Size(94, 29);
+            btnUpdataData.TabIndex = 7;
+            btnUpdataData.Text = "button2";
+            btnUpdataData.UseVisualStyleBackColor = true;
+            btnUpdataData.Click += btnUpdataData_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1015, 540);
+            Controls.Add(btnUpdataData);
+            Controls.Add(lbltest);
+            Controls.Add(dataGridView1);
             Controls.Add(cerrarPuertaBtn);
             Controls.Add(abrirPuertaBtn);
             Controls.Add(label1);
             Controls.Add(button1);
-            Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TabControl tabControl1;
-        private TabPage no;
-        private TabPage idTarjeta;
-        private TabPage propietario;
         private Label label1;
-        private TabPage horaEntrada;
-        private TabPage horaSalida;
-        private TabPage presente;
         private Button button1;
         private Button abrirPuertaBtn;
-        private TabPage isManual;
-        private TabPage rechazados;
         private Button cerrarPuertaBtn;
+        private DataGridView dataGridView1;
+        private Label lbltest;
+        private Button btnUpdataData;
+    }
+    partial class MainForm
+    {
+        private void InitializeComponent()
+        {
+            //this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            //this.ClientSize = new System.Drawing.Size(800, 450);
+            //this.Name = "MainForm";
+            //this.Text = "RFID Reader";
+            //this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            //this.ResumeLayout(false);
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,27 @@
+using System;
+using System.Data;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace proyecto_final
 {
     public partial class Form1 : Form
     {
-        System.IO.Ports.SerialPort Arduino;
-        //timer
-        private System.Windows.Forms.Timer timer;
+        private SerialPort Arduino;
+        private DataTable dataTable;
+        private StringBuilder dataBuffer = new StringBuilder();
+
         public Form1()
         {
             InitializeComponent();
-            InitializeComponent();
-            timer = new System.Windows.Forms.Timer();
-            // Intervalo en milisegundos (1 segundo)
-            timer.Interval = 1000;
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            InitializeDataTable();
 
             try
             {
-                Arduino = new System.IO.Ports.SerialPort("COM6", 9600);
+                Arduino = new SerialPort("COM3", 9600);
+                Arduino.DataReceived += SerialPort_DataReceived;
                 Arduino.Open();
             }
             catch (Exception ex)
@@ -25,39 +29,193 @@ namespace proyecto_final
                 MessageBox.Show($"Error al abrir el puerto COM: {ex.Message}");
             }
         }
-        private void Timer_Tick(object? sender, EventArgs e) // Agregar el modificador de nulabilidad para resolver CS8622
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            // Lógica del temporizador
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            try
+            {
+                string incomingData = Arduino.ReadExisting();
+                dataBuffer.Append(incomingData);
 
-        }
-
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+                // Procesar en un hilo de fondo
+                Task.Run(() => ProcessBufferedData());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al leer del puerto serial: {ex.Message}");
+            }
         }
 
-        private void fecha_Click(object sender, EventArgs e)
+        private void ProcessBufferedData()
         {
+            try
+            {
+                string data = dataBuffer.ToString();
+                int newLineIndex;
 
+                while ((newLineIndex = data.IndexOf('\n')) >= 0)
+                {
+                    string line = data.Substring(0, newLineIndex).Trim();
+                    dataBuffer.Remove(0, newLineIndex + 1);
+                    ProcessIncomingData(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al procesar datos: {ex.Message}");
+            }
+        }
+
+        private void ProcessIncomingData(string data)
+        {
+            try
+            {
+                string[] parts = data.Split(',');
+                if (parts.Length >= 4)
+                {
+                    string uid = parts[0].Replace("UID: ", "").Trim();
+                    string nameOrStatus = parts[1].Trim();
+                    string date = parts[2].Replace("Fecha: ", "").Trim();
+                    string time = parts[3].Replace("Hora: ", "").Trim();
+
+                    // Actualizar la UI de manera segura
+                    this.Invoke(new Action(() =>
+                    {
+                        lbltest.Text = $"UID: {uid}\nPropietario: {nameOrStatus}\nFecha: {date}\nHora: {time}";
+
+                        // Verificar si el UID ya existe en el DataTable
+                        if (!dataTable.AsEnumerable().Any(row => row.Field<string>("ID Tarjeta") == uid))
+                        {
+                            dataTable.Rows.Add(uid, nameOrStatus, date, time);
+                        }
+                    }));
+
+                    // Opcional: Muestra en la consola para depuración
+                    Console.WriteLine($"UID: {uid}, Propietario: {nameOrStatus}, Fecha: {date}, Hora: {time}");
+                }
+                else
+                {
+                    Console.WriteLine("Formato de datos incorrecto: " + data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al procesar datos: {ex.Message}");
+            }
+        }
+
+        private void InitializeDataTable()
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("ID Tarjeta", typeof(string));
+            dataTable.Columns.Add("Propietario", typeof(string));
+            dataTable.Columns.Add("Fecha", typeof(string));
+            dataTable.Columns.Add("Hora", typeof(string));
+
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnUpdateData_Click(object sender, EventArgs e)
+        {
+            if (Arduino != null && Arduino.IsOpen)
+            {
+                Arduino.Write("3");
+                UpdateDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("El puerto serial no está abierto o no está configurado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateDataGridView()
+        {
+            try
+            {
+                if (Arduino != null && Arduino.IsOpen)
+                {
+                    // Lee los datos del puerto serial
+                    string incomingData = Arduino.ReadLine()?.Trim(); // Lee una línea de datos y elimina espacios
+
+                    // Verifica si incomingData es nulo o vacío
+                    if (!string.IsNullOrEmpty(incomingData))
+                    {
+                        // Procesa los datos como lo harías normalmente
+                        ProcessIncomingData(incomingData);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se recibieron datos del puerto serial.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El puerto serial no está abierto o no está configurado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al actualizar datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Arduino != null && Arduino.IsOpen)
+            {
+                Arduino.Close();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Arduino.Write("1");
+            if (Arduino != null && Arduino.IsOpen)
+            {
+                Arduino.Write("1");
+            }
         }
 
         private void cerrrPuertaBtn_Click(object sender, EventArgs e)
         {
-            Arduino.Write("2");
-            //Arduino.ReadByte();
+            if (Arduino != null && Arduino.IsOpen)
+            {
+                Arduino.Write("2");
+            }
+        }
+
+        private void lbltest_Click(object sender, EventArgs e)
+        {
+            // Acción al hacer clic en el label (opcional)
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Acción al hacer clic en el DataGridView (opcional)
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            // Acción al hacer clic en la tab (opcional)
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Acción al hacer clic en el label (opcional)
+        }
+
+        private void updateDataClick(object sender, EventArgs e)
+        {
+            // Acción al hacer clic en el botón (opcional)
+        }
+
+        private void fecha_Click(object sender, EventArgs e)
+        {
+            // Acción al hacer clic en la fecha (opcional)
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Lógica adicional al cargar el formulario (opcional)
         }
     }
 }
